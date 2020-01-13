e8e490f4760f67adac6fb7ecfcdf7652c614c415
twopheat <twopheat@gmail.com>
2020-01-13 12:44:46 -0800

    Major fixes, got mostly working!

--- index.js
+++ index.js
@@ -1,51 +1,69 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-//-- Currently ONLY the pdf-generator works, really trying hard to get it to use a color trigger and import live data.
-exports.datA = {
-	title: "Coding Boot Camp",
-	date: "1/10/2020",
-	name: "Brad Johnston",
-	age: 40,
-	repositories: 13,
-	stars: 4,
-	following: 10,
-	followers: 8,
-	skills: "HTML CSS Mongo Express React Node Javascript",
-	story: "Currently attending the UC Riverside Extension - Full Stack Coding Boot Camp."
-}
+const pdfGen = require('./pdf-generator.js')
 
+/*const data = {
+  title: "Coding Boot Camp",
+  date: "1/10/2020",
+  name: "Brad Johnston",
+  age: 40,
+  repositories: 13,
+  stars: 4,
+  following: 10,
+  followers: 8,
+  skills: "HTML CSS Javascript Mongo Expres.datas React Node",
+  story: "Currently attending the UC Riverside Extension - Full Stack Coding Boot Camp."
+}*/
+var colors = { blue: "blue lighten-2", white: "white", black: "black" }
 inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
+  .prompt([{
+    message: "Enter Your GitHub Username:",
     name: "username"
-  })
-  .then(function ({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+  },
+  {
+    message: "Enter Your favorite Color:",
+    name: "color",
+    type: "list",
+    choices: ["blue", "white", "black"]
+  }])
+  .then(function ({ username, color }) {
+    let data = { color: colors[color] }
+    const queryUrl = `https://api.github.com/users/${username}`;
 
     axios.get(queryUrl).then(function (res) {
-      const repoNames = res.data.map(function (repo) {
-        return repo.name;
-      });
-      repos = repoNames.length;
-      console.log(`Saved ${repoNames.length} repos`);
-      //-------------------------
+      data["stars"] = res.data.starred_url.split("{")[0];
+      data["followers"] = res.data.followers;
+      data["following"] = res.data.following;
+      data["repositories"] = res.data.public_repos;
+      data["followers"] = res.data.followers;
+      data["location"] = res.data.location;
+      data["name"] = res.data.name;
+      data["bio"] = res.data.bio;
+      data["skills"] = res.data.skills;
 
 
-    })
+      stars(data);
+      console.log(data);
+    });
+
+   /* const data2 = {
+      name: "Brad Johnston",
+      skills: "HTML CSS Javascript Mongo Express React Node",
+      bio: "Currently attending the UC Riverside Extension - Full Stack Coding Boot Camp."
+    }*/
+
+  });
+
+async function stars(data) {
+  axios.get(data.stars).then(function (res) {
+    data.stars = res.data.length;
+    pdfGen(data);
   })
-  .then(function query2() {
+};
 
-    var child_process = require('child_process');
 
-    //EXECUTE query2.js
-    child_process.exec('node query2.js', (error, stdout, stderr) => {
-      console.log(`${stdout}`);
-      console.log(`${stderr}`);
-      if (error !== null) {
-        console.log(`exec error: ${error}`);
-      }
-    })
-  })
 
-query2(200);
+
+
+
--- pdf-generator.js
+++ pdf-generator.js
@@ -3,56 +3,64 @@ const path = require("path");
 const puppeteer = require('puppeteer');
 const handlebars = require("handlebars");
 
-async function createPDF(data){
+//const index = require("./index.")
 
-	var templateHtml = fs.readFileSync(path.join(process.cwd(), 'template.html'), 'utf8');
-	var template = handlebars.compile(templateHtml);
-	var html = template(data);
+	//-------------------------------------Buildout below.
+	async function createPDF(data) {
 
-	var timestamp = new Date();
-	timestamp = timestamp.getTime();
+		var templateHtml = fs.readFileSync(path.join(process.cwd(), 'template.html'), 'utf8');
+		var template = handlebars.compile(templateHtml);
+		var html = template(data);
 
-	var pdfPath = path.join('pdf', `${data.name}-${timestamp}.pdf`);
+		var timestamp = new Date();
+		timestamp = timestamp.getTime();
 
-	var options = {
-		width: '1230px',
-		headerTemplate: "<p></p>",
-		footerTemplate: "<p></p>",
-		displayHeaderFooter: false,
-		margin: {
-			top: "20px",
-			bottom: "30px"
-		},
-		printBackground: true,
-		path: pdfPath
+		var pdfPath = path.join('pdf', `${data.name}-${timestamp}.pdf`);
+
+		var options = {
+			width: '1230px',
+			headerTemplate: "<p></p>",
+			footerTemplate: "<p></p>",
+			displayHeaderFooter: false,
+			margin: {
+				top: "20px",
+				bottom: "30px"
+			},
+			printBackground: true,
+			path: pdfPath
+		}
+
+		const browser = await puppeteer.launch({
+			args: ['--no-sandbox'],
+			headless: true
+		});
+
+		var page = await browser.newPage();
+
+		await page.goto(`data:text/html;charset=UTF-8,${html}`, {
+			waitUntil: 'networkidle0'
+		});
+
+		await page.pdf(options);
+		await browser.close();
 	}
 
-	const browser = await puppeteer.launch({
-		args: ['--no-sandbox'],
-		headless: true
-	});
 
-	var page = await browser.newPage();
-	
-	await page.goto(`data:text/html;charset=UTF-8,${html}`, {
-		waitUntil: 'networkidle0'
-	});
 
-	await page.pdf(options);
-	await browser.close();
-}
+	const data = {
+		title: "Coding Boot Camp",
+		date: "1/10/2020",
+		name: "Brad Johnston",
+		repositories: 13,
+		color:"blue lighten-2",
+		username: "twopheat",
+		stars: 4,
+		following: 10,
+		followers: 8,
+		skills: "HTML CSS Javascript Mongo Express React Node",
+		bio: "Currently attending the UC Riverside Extension - Full Stack Coding Boot Camp."
+	}
 
-const data = {
-	title: "Coding Boot Camp",
-	date: "1/10/2020",
-	name: "Brad Johnston",
-	age: 40,
-	repositories: 13,
-	stars: 4,
-	following: 10,
-	followers: 8,
-	skills: "HTML CSS Mongo Express React Node Javascript",
-	story: "Currently attending the UC Riverside Extension - Full Stack Coding Boot Camp."
-}
 
-createPDF(data);
+module.exports = createPDF;
+//createPDF(data);
